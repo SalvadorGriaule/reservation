cd9771d2381ecaea4ba05689307ad1fc5ff79184
SalvadorGriaule <morales.quentin@gmx.fr>
2026-01-22 13:58:09 +0100

    add workflow

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trajet;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ReservationController extends Controller
 {
-    //
+    public function index() : View {
+        
+    }
 }
